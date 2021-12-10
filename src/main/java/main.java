921b8b85bbb1e276ef16ece81cd07115cbec8f2e
Chen <junbochen06@gmail.com>
2021-12-10 12:24:09 +0100

    this is only for dev branch

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,5 +2,8 @@ public class main {
     public static void main(String[] args) {
         System.out.println("HelloGITHUB");
         System.out.println("okok");
+
+        System.out.println("this is only for Dev");
     }
+
 }
